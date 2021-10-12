6e3f901fd11e8cedf55aac8683087031acef8370
AshwinKurup <akurup120@gmail.com>
2021-10-12 20:15:33 +0800

    Update admin.py

--- quizapi/admin.py
+++ quizapi/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import HufQuiz
 
-# Register your models here.
+# class HufQuizAdmin(admin.ModelAdmin):
+#   list = ('quizid','gameid','quiz_duration','quiz_max_score','quiz_description','no_of_qn','total_no_qn')
+
+admin.site.register(HufQuiz)
