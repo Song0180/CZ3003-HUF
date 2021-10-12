3a4ebb0651c05d92a659886bd61a363d29a6e107
AshwinKurup <akurup120@gmail.com>
2021-10-12 20:18:24 +0800

    added quizapi.urls

--- project_huf/urls.py
+++ project_huf/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gameapi.urls')),
-    path('', include('loginapi.urls'),)
+    path('', include('loginapi.urls'),),
+    path('', include('quizapi.urls')),
 ]
