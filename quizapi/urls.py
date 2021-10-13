5615e3b68477444618333e9fba3872b4f830983d
AshwinKurup <akurup120@gmail.com>
2021-10-13 16:57:44 +0800

    Update urls.py

--- quizapi/urls.py
+++ quizapi/urls.py
@@ -3,10 +3,11 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+router.register(r'quiz', views.HufQuizViewSet) # TODO ISSUE "Field 'total_no_qn' doesn't have a default value"
+router.register(r'quizoptions', views.HufQuizOptionViewSet) # TODO ISSUE "Unknown column 'huf_quiz_option.id' in 'field list'"
+router.register(r'quizqn', views.HufQuizQnViewSet)
+router.register(r'quizresult', views.HufQuizResultViewSet) # TODO ISSUE "Unknown column 'huf_quiz_result.id' in 'field list'"
+router.register(r'useransquiz', views.HufUserAnsViewSet) # TODO ISSUE  "Table 'project_huf.huf_user_ans' doesn't exist"
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
