cf19e97e2cdef5aba40e51ed48f6e29729fb80a3
Sok Ee <tengsokee@protonmail.com>
2021-10-15 01:31:19 +0800

    Popup when time runs out + comments

--- frontend/src/pages/GameplayPage/index.js
+++ frontend/src/pages/GameplayPage/index.js
@@ -1,10 +1,11 @@
-import * as React from 'react';
-import { GameplayDisplay } from '../../components/GameplayDisplay';
-import 'antd/dist/antd.css';
-import { Statistic, Row } from 'antd';
-import './index.css';
-
-import { useGameStore } from '../../services/zustand/game';
+import * as React from "react";
+import { GameplayDisplay } from "../../components/GameplayDisplay";
+import "antd/dist/antd.css";
+import { Statistic, Row } from "antd";
+import TimedPopUp from "../../components/TimedPopUp";
+import "./index.css";
+import { useEffect, useState } from "react";
+import { useGameStore } from "../../services/zustand/game";
 
 /*
   function to set the timer for the quiz
@@ -13,24 +14,29 @@ const { Countdown } = Statistic;
 const minutes = 1000 * 60 * 10; // Example, this is for 10 mins
 const deadline = Date.now() + minutes;
 
-//To show a pop up when the time is up (currently not done - have to edit so it does that)
-function onTimeUp() {
-  console.log('Times Up!');
-}
-
 /*
   function to include the components needed and display the information for Quiz Gameplay
 */
 
 const GameplayPage = () => {
   const { currentQuizQuetsions, fetchGameQuiz } = useGameStore();
-  const [userAnswers, setUserAnswers] = React.useState({});
+  const [userAnswers, setUserAnswers] = useState({});
+  const [timedPopUp, setTimedPopUp] = useState(false);
 
+  // To Spawn pop up message when the timer has run out of time
+  useEffect(() => {
+    setTimeout(() => {
+      setTimedPopUp(true);
+    }, minutes);
+  }, []);
+
+  // Fetches data of quiz using gameId
   React.useEffect(() => {
     const gameId = 0;
     fetchGameQuiz(gameId);
   }, [fetchGameQuiz]);
 
+  // Set options to be empty (when user loads the quiz page)
   React.useEffect(() => {
     const emptyAnswers = {};
     currentQuizQuetsions.forEach((question) => {
@@ -45,15 +51,16 @@ const GameplayPage = () => {
 
   return (
     <div>
-      <div className='header-container'>
-        <h2 style={{ color: 'orange' }}> CLOCKWORKS | Quiz 1 </h2>
-        <div className='timer-con'>
-          <Countdown title='Timer' value={deadline} onTimeUp={onTimeUp} />
+      <div className="header-container">
+        <h2 style={{ color: "orange" }}> CLOCKWORKS | Quiz 1 </h2>
+        <div className="timer-con">
+          <Countdown title="Timer" value={deadline} />
+          <TimedPopUp trigger={timedPopUp} setTrigger={setTimedPopUp} />
         </div>
       </div>
 
       <Row>
-        <div className='question-con'>
+        <div className="question-con">
           <GameplayDisplay
             quizQuestions={currentQuizQuetsions}
             currentAnswers={userAnswers}
@@ -66,8 +73,8 @@ const GameplayPage = () => {
         {Object.keys(userAnswers).map((questionId) => {
           return (
             <p key={questionId}>
-              QuestionId: {questionId}, AnswerValue:{' '}
-              {userAnswers[questionId] ?? 'No answer'}
+              QuestionId: {questionId}, AnswerValue:{" "}
+              {userAnswers[questionId] ?? "No answer"}
             </p>
           );
         })}
