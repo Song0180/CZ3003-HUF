9cd88eba8bb4cc62f9d592f0ac848d664056736a
Chi hui <chihui8199@gmail.com>
2021-10-15 22:14:24 +0800

    Remove tag UI, modify tag UI to look like form field

--- frontend/src/pages/GameCreationPage/index.js
+++ frontend/src/pages/GameCreationPage/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import "antd/dist/antd.css";
 import "./index.css";
-import { Tag, Tooltip, Form, Input, Button, InputNumber } from "antd";
-import { PlusOutlined } from "@ant-design/icons";
+import { Form, Input, Button, InputNumber } from "antd";
 import { Link } from "react-router-dom";
 
 const GameCreationPage = () => {
@@ -13,52 +12,6 @@ const GameCreationPage = () => {
 
   const onFinishFailed = (errorInfo) => {
     console.log("Failed:", errorInfo);
-  };
-
-  const [tags, setTags] = React.useState(["Games"]);
-  const [inputValue, setInputValue] = React.useState("");
-  const [inputVisible, setInputVisible] = React.useState(false);
-  const [editInputIndex, setEditInputIndex] = React.useState(-1);
-  const [editInputValue, setEditInputValue] = React.useState("");
-  const inputRef = React.useRef();
-  const saveEditInputRef = React.useRef();
-
-  const showInput = () => {
-    setInputVisible(true);
-    setTimeout(() => {
-      if (inputRef.current) {
-        inputRef.current.focus();
-      }
-    });
-  };
-
-  const handleInputChange = (value) => {
-    setInputValue(value);
-  };
-
-  const handleEditInputChange = (value) => {
-    setEditInputValue(value);
-  };
-
-  const handleInputConfirm = () => {
-    if (inputValue && tags.indexOf(inputValue) === -1) {
-      setTags([...tags, inputValue]);
-    }
-    setInputValue("");
-    setInputVisible(false);
-  };
-
-  const handleEditInputConfirm = () => {
-    const newTags = [...tags];
-    newTags[editInputIndex] = editInputValue;
-    setTags(newTags);
-    setEditInputIndex(-1);
-    setEditInputValue("");
-  };
-
-  const handleClose = (removeTag) => {
-    const newTags = tags.filter((tag) => tag !== removeTag);
-    setTags(newTags);
   };
 
   return (
@@ -116,6 +69,19 @@ const GameCreationPage = () => {
           </Form.Item>
 
           <Form.Item
+            label="INPUT GAME TAG"
+            name="Game Tag"
+            rules={[
+              {
+                required: true,
+                message: "Please input the Game Tag",
+              },
+            ]}
+          >
+            <Input placeholder="Enter Game Tag" />
+          </Form.Item>
+
+          <Form.Item
             label="NUMBER OF QUIZZES: "
             name="Number of Quizzes"
             rules={[
@@ -125,12 +91,7 @@ const GameCreationPage = () => {
               },
             ]}
           >
-            
-            <InputNumber
-              defaultValue={1}
-              min={1}
-              max={5}
-            />
+            <InputNumber defaultValue={1} min={1} max={5} />
           </Form.Item>
 
           <Form.Item
@@ -143,84 +104,7 @@ const GameCreationPage = () => {
               },
             ]}
           >
-            <InputNumber
-              defaultValue={1}
-              min={1}
-              max={10}
-            />
-          </Form.Item>
-
-          <Form.Item label="ENTER RELEVANT GAME TAGS">
-            <div>
-              {tags.map((tag, index) => {
-                if (editInputIndex === index) {
-                  return (
-                    <Input
-                      ref={saveEditInputRef}
-                      key={tag}
-                      size="small"
-                      className="tag-input"
-                      value={editInputValue}
-                      onChange={(e) => handleEditInputChange(e.target.value)}
-                      onBlur={handleEditInputConfirm}
-                      onPressEnter={handleEditInputConfirm}
-                    />
-                  );
-                }
-                const isLongTag = tag.length > 20;
-
-                const tagElem = (
-                  <Tag
-                    className="edit-tag"
-                    key={tag}
-                    closable={index !== 0}
-                    onClose={() => handleClose(tag)}
-                  >
-                    <span
-                      onDoubleClick={(e) => {
-                        if (index !== 0) {
-                          setEditInputIndex(index);
-                          setEditInputValue(tag);
-                          setTimeout(() => {
-                            if (saveEditInputRef.current) {
-                              saveEditInputRef.current.focus();
-                            }
-                          });
-                          e.preventDefault();
-                        }
-                      }}
-                    >
-                      {isLongTag ? `${tag.slice(0, 20)}...` : tag}
-                    </span>
-                  </Tag>
-                );
-                return isLongTag ? (
-                  <Tooltip title={tag} key={tag}>
-                    {tagElem}
-                  </Tooltip>
-                ) : (
-                  tagElem
-                );
-              })}
-
-              {inputVisible && (
-                <Input
-                  ref={inputRef}
-                  type="text"
-                  size="small"
-                  className="tag-input"
-                  value={inputValue}
-                  onChange={(e) => handleInputChange(e.target.value)}
-                  onBlur={handleInputConfirm}
-                  onPressEnter={handleInputConfirm}
-                />
-              )}
-              {!inputVisible && (
-                <Tag className="site-tag-plus" onClick={showInput}>
-                  <PlusOutlined /> New Tag
-                </Tag>
-              )}
-            </div>
+            <InputNumber defaultValue={1} min={1} max={10} />
           </Form.Item>
 
           <hr />
