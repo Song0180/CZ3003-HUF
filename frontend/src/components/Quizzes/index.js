52fb3d54d8fa5771db81228c591f82e5ae1cf683
Nickelodeon7856 <72136295+Nickelodeon7856@users.noreply.github.com>
2021-10-22 13:26:44 +0800

    API Integration

--- frontend/src/components/Quizzes/index.js
+++ frontend/src/components/Quizzes/index.js
@@ -1,9 +1,33 @@
 import * as React from 'react';
-import { Form, Input, InputNumber } from "antd";
+import { Form, Input, InputNumber, message } from "antd";
 import "./index.css";
 import { QuestionNumber } from "../../components/QuestionNumbers";
+import { useGameStore } from '../../services/zustand/game';
+import { useAuthStore } from '../../services/zustand/auth';
 
 const Quizzes = (props) => {
+
+  const { createNewQuiz } = useGameStore();
+  const { userInfo } = useAuthStore();
+
+  const onFinish = async (values) => {
+    const quizData = {
+      game_id: userInfo.game_id, //not sure if this is correct, what is the user has more than one game?
+      quiz_id: props.quizno,
+      quiz_duration: values.quiz_duration,
+      quiz_max_score: values.quiz_max_score,
+      quiz_description: values.quiz_description,
+      no_of_qn: props.NumberofQns,
+    };
+    const result = await createNewQuiz(quizData);
+    if (typeof result == 'string') {
+      message.error(result);
+    }
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
 
   return (
     <Form
@@ -18,14 +42,16 @@ const Quizzes = (props) => {
       remember: true,
     }}
 
-    autoComplete="off"
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete='off'
   >
 
     <p className="maintext">
         <span className="text-highlight">Quiz {props.quizno}</span>
       </p>
-    <p className="qntext">
-        <span className="qntext"> Level of Difficulty: {props.quizno}</span>
+      <p className="qntext">
+        <span className="qntext">Level of Difficulty: {props.quizno}</span>
       </p>
       <hr />
 
@@ -47,19 +73,31 @@ const Quizzes = (props) => {
       </Form.Item>
 
       <Form.Item
-        label="Duration (In Seconds)"
-        name="Duration (In Seconds)"
+        label="Duration (In Minutes): "
+        name="quiz_duration:"
         rules={[
           {
             required: true,
           },
         ]}
       >
-        <InputNumber min={1} max={3600} defaultValue={1}/> 
+        <InputNumber min={1} max={60} defaultValue={1}/> 
+      </Form.Item>
+
+      <Form.Item
+        label="Total Quiz Score: "
+        name="quiz_max_score:"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <InputNumber min={props.NumberofQns} max={props.NumberofQns} defaultValue={props.NumberofQns}/> 
       </Form.Item>
 
       <div>
-      <QuestionNumber qnNumber = {props.NumberofQns}/>
+      <QuestionNumber qnNumber = {props.NumberofQns} quiz_id = {props.quizno}/>
       </div>
 
     </Form>
