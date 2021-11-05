b325afb5aaac4be3b799afd61c3dd8fe1207a838
tengsokee <tengsokee@protonmail.com>
2021-11-06 00:03:25 +0800

    when user leave question blank, there will be an alert

--- frontend/src/components/GameplayDisplay/index.js
+++ frontend/src/components/GameplayDisplay/index.js
@@ -1,8 +1,8 @@
-import * as React from 'react';
-import { Radio, Space, Row, Button, Alert } from 'antd';
-import 'antd/dist/antd.css';
-import './index.css';
-import { useState } from 'react';
+import * as React from "react";
+import { Radio, Space, Row, Button, Form } from "antd";
+import "antd/dist/antd.css";
+import "./index.css";
+// import { useState } from "react";
 
 /*
 function to display the questions in the quiz and return the marks gotten (currenty not done)
@@ -20,100 +20,152 @@ const GameplayDisplay = ({
   };
 
   //Shows alert when not all question answered and show an alert
-  const [showAlert, setShowAlert] = useState(false);
-  // const onFinish=(e)=>{
-  //   console.log(e)
-  //   setTimeout(() =>{
-  //     setShowAlert(true)
-  //   }, 2000)
-  // }
+  const onFinish = (e) => {
+    ComputeScore();
+  };
 
   function ComputeScore() {
     var totalScore = 0;
-    // for (let i = 0; i < quizQuestions.length; i++) {
-    //   if (currentAnswers[i + 1].option_id === null) {
-    //     console.log("empty option")
-    //     break;
-    //   } else {
-    //     console.log(quizQuestions[i].quiz_qn_id);
-    //     console.log(quizQuestions[i].correct_ans);
-    //     console.log(currentAnswers[i + 1].option_id);
-    //     var correctAns = quizQuestions[i].correct_ans;
-    //     var chosen = currentAnswers[i + 1].option_id;
-    //     var score = quizQuestions[i].score_per_qn;
-    //     if (correctAns === chosen) {
-    //       totalScore = score + totalScore;
-    //     }
-    //   }
-    // }
     for (let i = 0; i < quizQuestions.length; i++) {
-      console.log(quizQuestions[i].quiz_qn_id);
-      console.log(quizQuestions[i].correct_ans);
-      console.log(currentAnswers[i + 1].option_id);
-      var correctAns = quizQuestions[i].correct_ans;
-      var chosen = currentAnswers[i + 1].option_id;
-      var score = quizQuestions[i].score_per_qn;
-      if (chosen === null) {
-        break;
-      } else if (correctAns === chosen) {
-        totalScore = score + totalScore;
+        // console.log(quizQuestions[i].quiz_qn_id);
+        console.log(quizQuestions[i].correct_ans);
+        // console.log(currentAnswers[i + 1].option_id);
+        var correctAns = quizQuestions[i].correct_ans;
+        var chosen = currentAnswers[i + 1].option_id;
+        var score = quizQuestions[i].score_per_qn;
+        if (correctAns === chosen) {
+          totalScore = score + totalScore;
+        }
       }
+      console.log('totalscore: ' + totalScore);
+      return totalScore;
     }
-    console.log('totalscore: ' + totalScore);
-    return totalScore;
-  }
+
+
 
   return (
-    <div className='question-container'>
-      {/* error when not all questions are answered but not implemented. */}
-      {showAlert && (
-        <Alert type='error' message='Not all questions are answered' closable />
-      )}
-      {quizQuestions.map((quiz_item, index) => {
-        return (
-          <div key={quiz_item.quiz_qn_id} className='question-container'>
-            <Row>
-              <h1>
-                Question {index + 1}) {quiz_item.question_name}
-              </h1>
-            </Row>
-            <Radio.Group
-              name='radiogroup'
-              onChange={(e) =>
-                handleOnChangeQuestionAnswer(
-                  quiz_item.quiz_qn_id,
-                  e.target.value
-                )
-              }
-              defaultValue={null}
-            >
-              <Space direction='vertical'>
-                {quiz_item.options.map((option_item) => {
-                  return (
-                    <Radio
-                      key={quiz_item.quiz_qn_id + option_item.value}
-                      value={option_item}
-                    >
-                      {option_item.option_description}
-                    </Radio>
-                  );
-                })}
-              </Space>
-            </Radio.Group>
-          </div>
-        );
-      })}
-      <br />
-      <div className='button-container'>
-        <Button
-          onClick={ComputeScore}
-          type='primary'
-          style={{ background: 'orange', borderColor: 'orange' }}
-        >
-          Finish Quiz
-        </Button>
-      </div>
+    <div className="question-container">
+      <Form
+        name="answer-questions"
+        onFinish={onFinish}
+      >
+        {quizQuestions.map((quiz_item, index) => {
+          return (
+            <div key={quiz_item.quiz_qn_id} className="question-container">
+              <Row>
+                <h1>
+                  Question {index + 1}) {quiz_item.question_name}
+                </h1>
+              </Row>
+              <Radio.Group
+                name="radiogroup"
+                onChange={(e) =>
+                  handleOnChangeQuestionAnswer(
+                    quiz_item.quiz_qn_id,
+                    e.target.value
+                  )
+                }
+                initialValues={null}
+              >
+                <Form.Item
+                  name="Answer"
+                  rules={[
+                    {
+                      required: true,
+                    },
+                  ]}
+                >
+                  <Space direction="vertical">
+                    {quiz_item.options.map((option_item) => {
+                      return (
+                        <Radio
+                          key={quiz_item.quiz_qn_id + option_item.value}
+                          value={option_item}
+                        >
+                          {option_item.option_description}
+                        </Radio>
+                      );
+                    })}
+                  </Space>
+                </Form.Item>
+              </Radio.Group>
+            </div>
+          );
+        })}
+
+        <Form.Item className="button-container">
+          <Button
+            type="primary"
+            htmlType="submit"
+            style={{ background: "orange", borderColor: "orange" }}
+          >
+            Finish Quiz
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
+
+    // <div className="question-container">
+    //   {quizQuestions.map((quiz_item, index) => {
+    //     return (
+    //       <div key={quiz_item.quiz_qn_id} className="question-container">
+    //         <Row>
+    //           <h1>
+    //             Question {index + 1}) {quiz_item.question_name}
+    //           </h1>
+    //         </Row>
+    //         <Form
+    //           name="control-hooks"
+    //           onFinish={onFinish}
+    //           onFinishFailed={onFinishFailed}
+    //         >
+    //           <Form.Item
+    //             name="Answer"
+    //             rules={[
+    //               {
+    //                 required: true,
+    //               },
+    //             ]}
+    //           >
+    //             <Radio.Group
+    //               name="radiogroup"
+    //               onChange={(e) =>
+    //                 handleOnChangeQuestionAnswer(
+    //                   quiz_item.quiz_qn_id,
+    //                   e.target.value
+    //                 )
+    //               }
+    //               initialValues={null}
+    //             >
+    //               <Space direction="vertical">
+    //                 {quiz_item.options.map((option_item) => {
+    //                   return (
+    //                     <Radio
+    //                       key={quiz_item.quiz_qn_id + option_item.value}
+    //                       value={option_item}
+    //                     >
+    //                       {option_item.option_description}
+    //                     </Radio>
+    //                   );
+    //                 })}
+    //               </Space>
+    //             </Radio.Group>
+    //           </Form.Item>
+    //           <Form.Item className="button-container">
+    //             <Button
+    //               type="primary"
+    //               htmlType="submit"
+    //               style={{ background: "orange", borderColor: "orange" }}
+    //             >
+    //               Finish Quiz
+    //             </Button>
+    //           </Form.Item>
+    //         </Form>
+    //       </div>
+    //     );
+    //   })}
+    //   <br />
+    // </div>
   );
 };
 
